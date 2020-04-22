e8869950b8fec32815180c2ae410f3ec95173517
shonburgundy <seanbeloussow@yahoo.com>
2020-04-22 14:56:33 -0700

    add test file

---
+++ __tests__/cocktails.test.js
@@ -0,0 +1,7 @@
+import { CocktailService } from './../src/cocktails.js'
+
+// describe ("Cocktail Service", () => {
+//   test("should return a list of ingredients retrieved from the API call", () => {
+//     let cocktailService = new CocktailService;
+//   })
+// })
