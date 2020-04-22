60123506ada6054b1504ec9052a7be7886ebb6d4
shonburgundy <seanbeloussow@yahoo.com>
2020-04-22 14:57:03 -0700

    create gitIngredientArray method

--- src/main.js
+++ src/main.js
@@ -8,16 +8,31 @@ import { CocktailService } from './../src/cocktails';
 $(document).ready(function() {
   $("#cocktailSearch").click(function() {
     let drink = $("#cocktailInput").val();
-    console.log(drink);
-
     (async () => {
       let cocktailService = new CocktailService();
       const response = await cocktailService.getCocktailIngredients(drink);
       getElements(response);
+      getIngredientArray(response);
     })();
 
+    function getIngredientArray(response) {
+      if (response) {
+        let drinkArray = response.drinks[0];
+        let valueArray = Object.values(drinkArray);
+        console.log(valueArray);
+        console.log(valueArray[21]);
+        let finalIngredientArray = [];
+        for (let i = 21; i < 36; i++) { 
+          if(valueArray[i]) {
+            finalIngredientArray.push(valueArray[i])
+          }
+        }
+        console.log(finalIngredientArray);
+        return finalIngredientArray;
+      }
+    }
+
     function getElements(response) {
-      console.log(response);
       if (response) {
         //LOOK FOR JSON KEY VALUE NAMES TO REFERENCE INGREDIENTS
         $(".showIngredients").text(`The ingredients for a ${drink} are ${response.drinks[0].strIngredient1}`);
